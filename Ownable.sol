3fd4f9e056cfdb1df78ca862db9a7f80d3e78919
unknown <z5208980@ad.unsw.edu.au>
2024-03-26 19:34:11 +1100

    init

---
+++ Ownable.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.20;
+
+contract Ownable {
+    address private owner;
+
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not owner");
+        _;
+    }
+
+    constructor(address _owner) {
+        owner = _owner; // 'msg.sender' is sender of current call, contract deployer for a constructor
+        emit OwnerSet(address(0), owner);
+    }
+
+    function changeOwner(address newOwner) public onlyOwner {
+        emit OwnerSet(owner, newOwner);
+        owner = newOwner;
+    }
+
+    function getOwner() external view returns (address) {
+        return owner;
+    }
+}
---
+++ SolidityDatabaseRegistry.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.20;
+
+import "./Ownable.sol";
+
+contract SolidityDatabaseRegistry is Ownable {
+    struct ContractInfo {
+        string id;
+        uint256 timestamp;
+    }
+
+    mapping(string => ContractInfo) bytecodeDB;
+
+    constructor() Ownable(msg.sender) {}
+
+    function find(string memory hash)
+        public
+        view
+        returns (ContractInfo memory)
+    {
+        return bytecodeDB[hash];
+    }
+
+    /**
+     * @dev allows the service to add data
+     */
+    function add(string memory hash, string memory cid) public onlyOwner {
+        require(
+            bytes(bytecodeDB[hash].id).length == 0,
+            "Contract already added"
+        );
+
+        ContractInfo memory newContract;
+        newContract.id = cid;
+        newContract.timestamp = block.number;
+
+        bytecodeDB[hash] = newContract;
+    }
+
+    /**
+     * @dev allows the service to add data multi
+     */
+    function adds(string[] memory hashes, string memory cid) public onlyOwner {
+        for (uint256 i = 0; i < hashes.length; i++) {
+            add(hashes[i], cid);
+        }
+    }
+
+    /**
+     * @dev Note: This should never to called unless something happens
+     */
+    function addOverride(string memory hash, string memory cid)
+        public
+        onlyOwner
+    {
+        ContractInfo memory newContract;
+        newContract.id = cid;
+        newContract.timestamp = block.number;
+
+        bytecodeDB[hash] = newContract;
+    }
+}
